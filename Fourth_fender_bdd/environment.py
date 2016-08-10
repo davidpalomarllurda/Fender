7e4dc518a4754bd0bd9d223b9bb49120c973d715
David Palomar <david.palomarllurda@gmail.com>
2016-08-10 13:19:48 +0100

    Added implicit wait time for the bdd exercise

--- Fourth_fender_bdd/environment.py
+++ Fourth_fender_bdd/environment.py
@@ -10,6 +10,9 @@ from selenium import webdriver
 def before_scenario(context, scenario):
     
     context.driver = webdriver.Chrome()
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(30)
+    
 
 def after_scenario(context, scenario):
     context.driver.quit()
